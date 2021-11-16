1c8524592c036ad11a4daaa96999aae2b54579f4
João Marcos Chaves <joaomarcoschaves7@gmail.com>
2021-11-16 17:58:48 -0300

    Adicionando rota de exemplo

--- Config/Server.js
+++ Config/Server.js
@@ -1,14 +1,17 @@
+
 const express = require('express')
 const bodyParser = require('body-parser')
-const allowCors = require('cors')
+const allowCors = require('./Cors')
+let MainRoutes = require('./MainRoutes')
 const server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(MainRoutes)
 
 server.listen(3000, function(){
-	console.log('Server rodando na porta 3000')
+	console.log('Server online on 3000')
 })
 
 module.exports = server
